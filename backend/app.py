3b3b00e2f4ffc10b95f6ee09ac68123f91469a54
Ramzia-zia <christelletchac@gmail.com>
2026-03-09 00:47:20 +0000

    Fix port pour Railway

--- backend/app.py
+++ backend/app.py
@@ -228,4 +228,4 @@ scheduler.add_job(envoyer_notifications, 'interval', minutes=1)
 scheduler.start()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)), debug=False)
